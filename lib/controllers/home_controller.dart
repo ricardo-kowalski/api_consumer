da99dccb8cc0a189619585a930acb9d4f9bec24d
Ricardo <kowalski.kgb@gmail.com>
2020-12-22 21:38:50 -0300

    reatividade

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,11 +1,21 @@
 import 'package:api_consumer/models/api_users.dart';
 import 'package:api_consumer/repositories/user_repository.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeController {
   List<ApiUsers> dados = [];
   final repository = UserRepository();
+  final state = ValueNotifier<HomeState>(HomeState.start);
 
   Future start() async {
-    dados = await repository.fetchUsers();
+    state.value = HomeState.loading;
+    try {
+      dados = await repository.fetchUsers();
+      state.value = HomeState.sucess;
+    } catch (e) {
+      state.value = HomeState.error;
+    }
   }
 }
+
+enum HomeState { start, loading, sucess, error }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:api_consumer/controllers/home_controller.dart';
 import 'package:api_consumer/widgets/user_card.dart';
 import 'package:api_consumer/models/api_users.dart';
 import 'package:flutter/material.dart';
@@ -11,55 +12,100 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<ApiUsers> _api;
-  bool carregado = false;
+  // List<ApiUsers> _api;
 
-  Future<List<ApiUsers>> _getUser() async {
-    try {
-      List<ApiUsers> listUser = List();
-      final response =
-          await http.get('https://jsonplaceholder.typicode.com/users');
-      if (response.statusCode == 200) {
-        var decodeJson = jsonDecode(response.body);
-        decodeJson.forEach((item) => listUser.add(ApiUsers.fromJson(item)));
-        return listUser;
-      } else {
-        print("Erro ao carregar lista");
-        return null;
-      }
-    } catch (e) {
-      print("Erro ao carregar lista");
-      return null;
+  final controller = HomeController();
+
+  _sucess() {
+    return ListView.builder(
+      itemCount: controller.dados.length,
+      itemBuilder: (context, index) {
+        var dado = controller.dados[index];
+        return Container(
+          margin: EdgeInsets.all(10),
+          height: 80,
+          color: Colors.blue,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(dado.name),
+                Text(dado.email),
+                Text(dado.phone),
+                Text(dado.company.name),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  _error() {
+    return Center(
+      child: RaisedButton(
+        onPressed: () {
+          controller.start();
+        },
+        child: Text("Tentar Novamente"),
+      ),
+    );
+  }
+
+  _loading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  _start() {
+    return Container(child: Text("teste"));
+  }
+
+  stateManagement(HomeState state) {
+    switch (state) {
+      case HomeState.start:
+        return _start();
+      case HomeState.loading:
+        return _loading();
+      case HomeState.error:
+        return _error();
+      case HomeState.sucess:
+        return _sucess();
+      default:
+        return _start();
     }
   }
 
   @override
   void initState() {
     super.initState();
-    _getUser().then((map) {
-      setState(() {
-        _api = map;
-        carregado = true;
-      });
-      // print(_api.length);
-    });
+    controller.start();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh_outlined),
+            onPressed: () {
+              controller.start();
+            },
+          )
+        ],
         centerTitle: true,
         title: Text("HomeScreen"),
       ),
-      body: carregado
-          ? UserCard(_api)
-          : Center(
-              child: CircularProgressIndicator(),
-            ),
+      body: AnimatedBuilder(
+        animation: controller.state,
+        builder: (contex, child) {
+          return stateManagement(controller.state.value);
+        },
+      ),
     );
   }
 }
-
-// CONTINUAR A PARTIR DAQUI
-// https://www.youtube.com/watch?v=ugw3s8plVDk&list=PLlBnICoI-g-d-J57QIz6Tx5xtUDGQdBFB&index=47
